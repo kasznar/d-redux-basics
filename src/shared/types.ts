33729601c3d940ee7b024f603ede9b24ca782604
Kasznár Dávid <kasznardavid8@gmail.com>
2021-10-13 23:08:47 +0200

    base project

--- src/shared/types.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export type InputEvent = React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
--- src/store/reducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import commentsSlice from "../features/comments/commentsSlice";
-
-const rootReducer = combineReducers({
-    // Define a top-level state field named `todos`, handled by `todosReducer`
-    comments: commentsSlice,
-})
-
-export default rootReducer
